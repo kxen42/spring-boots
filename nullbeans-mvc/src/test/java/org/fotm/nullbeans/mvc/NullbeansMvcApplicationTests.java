2e1494bdc3d100eb9d364ba6a9867e69f05af6db
kxen42 <kxen42@users.noreply.github.com>
2023-01-17 21:26:29 -0600

    New module nullbeans-mvc

---
+++ nullbeans-mvc/src/test/java/org/fotm/nullbeans/mvc/NullbeansMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fotm.nullbeans.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NullbeansMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
